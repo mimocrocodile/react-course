07c939d36c262236fbb2eaac12b23ff1067394a9
john <oxnardmontana@gmail.com>
2023-05-23 00:54:24 +0600

    github pages added

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
   return (
     <nav>
       <div className="nav-wrapper">
-        <a href="/" className="brand-logo">
+        <a href="." className="brand-logo">
           Logo
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
