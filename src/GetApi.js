5b8e766e4066e64024a040b20eb4280e905b5c44
mimocrocodile <73649418+mimocrocodile@users.noreply.github.com>
2023-05-24 19:28:01 +0300

    Update GetApi.js

--- src/GetApi.js
+++ src/GetApi.js
@@ -11,5 +11,5 @@ const apiLink =
   `https://api.themoviedb.org/3/discover/movie?include_adult=true&api_key=${API_KEY}&page=1`;
 const imgLink = "https://image.tmdb.org/t/p/original/";
 const searchMovie =
-  `https://api.themoviedb.org/3/search/movie?include_adult=true&sort_by=vote_average.desc&api_key=${API_KEY}}&query=`;
+  `https://api.themoviedb.org/3/search/movie?include_adult=true&sort_by=vote_average.desc&api_key=${API_KEY}&query=`;
 export { getApi, apiLink, imgLink, searchMovie };
